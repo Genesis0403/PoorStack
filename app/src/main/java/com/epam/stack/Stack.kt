7ff33ce4f6a510346798a87133b7b196f45035c8
Vlad Korotkevich <vlad.korotkevich2@gmail.com>
2019-04-04 13:33:20 +0300

    AbstractStack removed, replaced with ImmutableStack and MutableStack.

--- app/src/main/java/com/epam/stack/Stack.kt
+++ app/src/main/java/com/epam/stack/Stack.kt
@@ -1,28 +1,81 @@
 package com.epam.stack
 
-abstract class AbstractStack<T> : Collection<T> {
+import java.lang.IllegalArgumentException
+
+interface ImmutableStack<T> : Collection<T> {
+    override var size: Int
+
+    override fun contains(element: T): Boolean
+
+    override fun containsAll(elements: Collection<T>): Boolean
+
+    override fun isEmpty(): Boolean
+
+    override fun iterator(): Iterator<T>
+}
+
+interface MutableStack<T> : ImmutableStack<T>, MutableCollection<T> {
+
+    fun pop(): T
+
+    fun peek(): T?
+
+    override fun add(element: T): Boolean
+
+    override fun addAll(elements: Collection<T>): Boolean
+
+    override fun clear()
+
+    override fun remove(element: T): Boolean
+
+    override fun removeAll(elements: Collection<T>): Boolean
+
+    override fun retainAll(elements: Collection<T>): Boolean
+}
+
+class Stack<T> : MutableStack<T>, MutableCollection<T> {
 
     override var size: Int = 0
 
-    override fun contains(element: T) = iterator().asSequence().contains(element)
-
-    override fun containsAll(elements: Collection<T>)
-            = iterator().asSequence().all { el -> elements.contains(el)}
-
-    override fun isEmpty() = size == 0
-
-    abstract fun peek(): T?
-}
-
-class Stack<T> : AbstractStack<T>() {
-
     private var top: Node<T>? = null
 
-    class Node<T>(val el: T, val prev: Node<T>?)
+    class Node<T>(val el: T, var prev: Node<T>?)
 
-    override fun peek(): T? = top?.el
+    override fun add(element: T): Boolean {
+        top = Node(element, top)
+        return true
+    }
 
-    inner class Itr : Iterator<T> {
+    override fun pop(): T {
+        if (top == null) throw IllegalArgumentException()
+        val prev = top!!.prev
+        val el = top!!.el
+        top!!.prev = null
+        top = prev
+        return el
+    }
+
+    override fun peek(): T? {
+        return top?.el
+    }
+
+    override fun clear() {
+        while (top != null) {
+            val prev = top?.prev
+            top?.prev = null
+            top = prev
+        }
+        size = 0
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        for (el in elements) {
+            add(el)
+        }
+        return true
+    }
+
+    inner class Itr : MutableIterator<T> {
 
         private var cursor = top
 
@@ -33,7 +86,32 @@ class Stack<T> : AbstractStack<T>() {
             cursor = cursor?.prev
             return temp!!.el
         }
+
+        override fun remove() {
+            throw UnsupportedOperationException()
+        }
     }
 
-    override fun iterator() = Itr()
+    override fun iterator(): MutableIterator<T> = Itr()
+
+    override fun contains(element: T) = iterator().asSequence().contains(element)
+
+    override fun containsAll(elements: Collection<T>)
+            = iterator().asSequence().all { el -> elements.contains(el)}
+
+    override fun isEmpty() = size == 0
+
+    override fun remove(element: T): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    override fun removeAll(elements: Collection<T>): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    override fun retainAll(elements: Collection<T>): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    //TODO implement toStack()
 }
--- app/src/test/java/com/epam/poorstack/MutableStackTest.kt
+++ app/src/test/java/com/epam/poorstack/MutableStackTest.kt
@@ -1,14 +1,17 @@
 package com.epam.poorstack
 
 import com.epam.stack.MutableStack
-import org.junit.Test
+import com.epam.stack.Stack
+import org.junit.Assert
 import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Test
 
 class MutableStackTest {
 
     @Test
     fun `whenGivenStack pushElements thenPop`() {
-        val stack: MutableStack<String> = MutableStack()
+        val stack: MutableStack<String> = Stack()
         stack.apply {
             add("hello")
             add("world")
@@ -19,11 +22,22 @@ class MutableStackTest {
 
     @Test
     fun `whenGivenStack pushElements thenPeek`() {
-        val stack: MutableStack<String> = MutableStack()
+        val stack: MutableStack<String> = Stack()
         stack.apply {
             add("hello")
             add("world")
         }
         assertEquals("world", stack.peek())
+    }
+
+    @Test
+    fun `whenGivenStack popElement thenThrowException`() {
+        val stack: MutableStack<String> = Stack()
+        try {
+            stack.pop()
+            fail()
+        } catch (e: Exception) {
+            Assert.assertTrue(true)
+        }
     }
 }
